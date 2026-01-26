d74c5d36de1443fbb179e890c0e31db2c537cc9e
prashantbhadu <prashantbhadu97@gmail.com>
2026-01-26 12:48:24 +0530

    Time: 3 ms (86.04%) | Memory: 15.5 MB (48.49%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -11,6 +11,24 @@ public:
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        return (solve(n-1,0,target,nums));
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        if((sum+target)%2!=0 || abs(target)>sum) return 0;
+        int newk= (sum+target)/2;
+        vector<vector<int>>dp(n,vector<int>(newk+1,0));
+        if( nums[0]==0) dp[0][0]=2;
+        else dp[0][0]=1;
+        if(newk>=nums[0] && nums[0]!=0) dp[0][nums[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int k=0;k<=newk;k++){
+                int not_take=dp[i-1][k];
+                int take=0;
+                if(k>=nums[i]) take=dp[i-1][k-nums[i]];
+                dp[i][k]= take + not_take;
+            }
+        }
+        return dp[n-1][newk];
     }
 };
