7470d3dd24d88c722063cd7a66d44ad4e47068a5
prashantbhadu <prashantbhadu97@gmail.com>
2026-02-02 16:57:14 +0530

    Time: 1708 ms (5.00%) | Memory: 41.1 MB (10.45%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, vector<string>> adj;
+        for(int j=0;j<wordList.size();j++){
+                string s=beginWord,t=wordList[j];
+                int count=0;
+                for(int k=0;k<s.size();k++){
+                    if(s[k]!=t[k]) count++;
+                }
+                if(count==1){
+                    adj[beginWord].push_back(wordList[j]);
+                    adj[wordList[j]].push_back(beginWord);
+                }
+            }
+        for(int i=0;i<wordList.size();i++){
+            for(int j=i+1;j<wordList.size();j++){
+                string s=wordList[i],t=wordList[j];
+                int count=0;
+                for(int k=0;k<s.size();k++){
+                    if(s[k]!=t[k]) count++;
+                }
+                if(count==1){
+                    adj[wordList[i]].push_back(wordList[j]);
+                    adj[wordList[j]].push_back(wordList[i]);
+                }
+            }
+        }
+        int count=1;
+        queue<string>q;
+        unordered_map<string, int> visited;
+        q.push(beginWord);
+        visited[beginWord]=1;
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                string str=q.front();
+                q.pop();
+                for(auto it:adj[str]){
+                    if(visited[it]!=1){
+                        q.push(it);
+                        visited[it]=1;
+                        if(it==endWord) return count+1;
+                    }
+                }
+            }
+            count++;
+        }
+        return 0;
+    }
+};
