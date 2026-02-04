d10d6334d31c3a534b8e4f0ec8cf843c30fee5a1
prashantbhadu <prashantbhadu97@gmail.com>
2026-02-04 07:43:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (24.39%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    bool dfs(int ind,int rang,vector<int>&color,vector<vector<int>>&graph){
-        color[ind]=rang;
-        for(auto it:graph[ind]){
-            if(color[it]== -1){
-                if(dfs(it,!rang,color,graph)==false) return false;
+    bool dfs(int ind,vector<int>&visited,vector<int>&temp,vector<vector<int>>&graph){
+        visited[ind]=1;
+        queue<pair<int,int>>q;
+        q.push({ind,-1});
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                pair<int,int>p=q.front();
+                q.pop();
+                int node=p.first;
+                int parent=p.second;
+                for(auto it:graph[node]){
+                    if(!visited[it]){
+                        q.push({it,node});
+                        visited[it]=1;
+                        temp[it]=temp[node]+1;
+                    }
+                    else if(parent!=it){
+                        int len=abs(temp[it]-temp[node])+1;
+                        if(len%2!=0) return false;
+                    }
+                }
             }
-            else if(color[it]==rang) return false;
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        vector<int>color(graph.size());
+        vector<int>visited(graph.size(),0);
+        vector<int>temp(graph.size(),0);
         for(int i=0;i<graph.size();i++){
-            color[i]=-1;
-        }
-        for(int i=0;i<graph.size();i++){
-            if(color[i]==-1){
-                if(dfs(i,0,color,graph)==false){
+            if(!visited[i]){
+                if(dfs(i,visited,temp,graph)==false){
                     return false;
                 }
             }
