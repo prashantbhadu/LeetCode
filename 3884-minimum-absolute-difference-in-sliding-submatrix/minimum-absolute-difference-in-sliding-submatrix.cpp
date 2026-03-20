fef5749fe03463f1a9bdced53303b3a9721b0af2
prashantbhadu <prashantbhadu97@gmail.com>
2026-03-20 16:42:33 +0530

    Time: 16 ms (64.44%) | Memory: 32.1 MB (56.30%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -19,27 +19,29 @@ public:
 }
     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
        
-        vector<vector<int>>ans;
-        
-        
-        int r=grid.size();
-        int col=grid[0].size();
-        for(int i=0;i<(r-k)+1;i++){
-            vector<int>result;
-            for(int j=0;j<(col-k)+1;j++){
-               set<int>store;
-                for(int l=i;l<k+i;l++){
-                    for(int m=j;m<k+j;m++){
+         int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> ans(m-k+1,vector<int>(n-k+1));
 
-                        store.insert(grid[l][m]);
+        for(int i=0;i<=m-k;i++){
+            for(int j=0;j<=n-k;j++){
+                vector<int> temp;
+
+                for(int r = i;r<i+k;r++){
+                    for(int c = j;c<j+k;c++){
+                        temp.push_back(grid[r][c]);
                     }
                 }
-                
-                int diff=func(store);
-                diff==INT_MAX?result.push_back(0):result.push_back(diff);
-
+                sort(begin(temp),end(temp));
+                int diff = INT_MAX;
+                for(int x=0;x<temp.size()-1;x++){
+                    if(temp[x]!=temp[x+1]){
+                        diff = min(diff,abs(temp[x]-temp[x+1]));
+                    }
+                }
+                if(diff==INT_MAX) ans[i][j]=0;
+                else ans[i][j]=diff;
             }
-            ans.push_back(result);
         }
         return ans;
     }
