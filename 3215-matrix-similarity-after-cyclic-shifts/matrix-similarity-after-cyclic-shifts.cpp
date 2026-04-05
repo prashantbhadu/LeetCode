6973936d73a94a05dbb35f36ae1021b3c9f0d4a3
prashantbhadu <prashantbhadu97@gmail.com>
2026-04-05 16:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (34.46%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        vector<vector<int>>ans(mat.size(),vector<int>(mat[0].size(),0));
         int l=mat[0].size();
         int col=k%l;
         for(int i=0;i<mat.size();i++){
