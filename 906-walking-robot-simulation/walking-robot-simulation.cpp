acb672076d6e84e628f31c454d8eb57716a33fcd
prashantbhadu <prashantbhadu97@gmail.com>
2026-04-06 22:45:45 +0530

    Time: 23 ms (66.40%) | Memory: 37.8 MB (56.53%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        set<pair<int,int>>blocked;
+        for(auto&o: obstacles){
+            blocked.insert({o[0],o[1]});
+        }
+        vector<pair<int,int>>directions ={
+            {0,1}, {1,0}, {0,-1}, {-1,0}
+        };
+        int x=0;
+        int y=0;
+        int dir=0;
+        int maxdist=0;
+        for(int cmd:commands){
+            if(cmd==-1) dir=(dir+1)%4;
+            else if(cmd==-2) dir=(dir+3)%4;
+            else{
+                while(cmd--){
+                    int nx=x+directions[dir].first;
+                    int ny=y+directions[dir].second;
+                    if(blocked.count({nx,ny})) break;
+                    x=nx;
+                    y=ny;
+                    maxdist=max(maxdist,x*x + y*y);
+                }
+            }
+        }
+        return maxdist;
+    }
+};
