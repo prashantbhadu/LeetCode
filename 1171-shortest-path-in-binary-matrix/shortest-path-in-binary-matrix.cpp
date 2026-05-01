1a3d235c2731f0bd53d19f44057415aecb553233
prashantbhadu <prashantbhadu97@gmail.com>
2026-05-01 16:48:31 +0530

    Time: 81 ms (12.42%) | Memory: 44 MB (9.06%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,32 +3,30 @@ public:
     int dx[8]={-1,-1,-1,0,0,1,1,1};
     int dy[8]={-1,0,1,-1,1,-1,0,1};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        if(grid[0][0]!=0 || grid[n-1][m-1]!=0) return -1;
-        vector<vector<int>>distance(n,vector<int>(m,1e9));
-        priority_queue<pair<int,int>, vector<pair<int,int>>, 
-                       greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        distance[0][0]=1;
+        int r=grid.size();
+        int c=grid[0].size();
+        if(grid[0][0]!=0 ||grid[r-1][c-1]!=0) return -1;
+        queue<vector<int>>pq;
+        vector<vector<int>>visit(r,vector<int>(c,0));
+        int cnt=1;
+        pq.push({0,0,cnt});
+        visit[0][0]=1;
+        
         while(!pq.empty()){
-            pair<int,int>p=pq.top();
-            int kd=p.first;
-            int dn=p.second;
+            auto temp=pq.front();
             pq.pop();
+            int x=temp[0];
+            int steps=temp[2];
+            int y=temp[1];
+            if(x==r-1 && y==c-1) return steps;
             for(int i=0;i<8;i++){
-                int x=p.first+dx[i];
-                int y=p.second+dy[i];
-                if(x>=0 && y>=0 && x<n && y<m && grid[x][y]==0){
-                    if(distance[kd][dn]+1<distance[x][y]){
-                        distance[x][y]=distance[kd][dn]+1;
-                        pq.push({x,y});
-                    }
-                }
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx<0 || ny<0 || nx>=r || ny>=c || visit[nx][ny] || grid[nx][ny]==1) continue;
+                pq.push({nx,ny,steps+1});
+                visit[nx][ny]=1;
             }
-        }  
-        if(distance[n-1][m-1]==1e9) return -1;
-        return distance[n-1][m-1];             
-
+        }
+        return -1;
     }
 };
