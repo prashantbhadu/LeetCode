916c833197f52b978ac3f5e41ef25edfc9acdffd
prashantbhadu <prashantbhadu97@gmail.com>
2026-05-03 03:15:41 +0530

    Time: 186 ms (67.83%) | Memory: 115.5 MB (66.49%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -34,7 +34,7 @@ public:
         }
     }
 
-    void unionBySize(int u, int v) {
+    void unionBysize(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
         if (ulp_u == ulp_v) return;
@@ -49,58 +49,50 @@ public:
     }
 };
 class Solution {
-private:
-    bool isValid(int newr, int newc, int n) {
-        return newr >= 0 && newr < n && newc >= 0 && newc < n;
-    }
 public:
-     int largestIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        DisjointSet ds(n * n);
-        // step - 1
-        for (int row = 0; row < n ; row++) {
-            for (int col = 0; col < n ; col++) {
-                if (grid[row][col] == 0) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n) && grid[newr][newc] == 1) {
-                        int nodeNo = row * n + col;
-                        int adjNodeNo = newr * n + newc;
-                        ds.unionBySize(nodeNo, adjNodeNo);
+    int dx[4]={0,0,1,-1};
+    int dy[4]={1,-1,0,0};
+    int largestIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        DisjointSet dsu(n*n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0) continue;
+                for(int k=0;k<4;k++){
+                    int x=i+dx[k];
+                    int y=j+dy[k];
+                    if(x>=0&&x<n&&y>=0&&y<n&&grid[x][y]==1){
+                        int nodeno=i*n+j;
+                        int adjnode=x*n+y;
+                        dsu.unionBysize(nodeno,adjnode);
+
                     }
                 }
             }
         }
-        // step 2
-        int mx = 0;
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (grid[row][col] == 1) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                set<int> components;
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n)) {
-                        if (grid[newr][newc] == 1) {
-                            components.insert(ds.findUPar(newr * n + newc));
-                        }
+        int maxi=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) continue;
+                set<int>components;
+                for(int k=0;k<4;k++){
+                    int x=i+dx[k];
+                    int y=j+dy[k];
+                    if(x>=0&&x<n&&y>=0&&y<n&&grid[x][y]==1){
+                        components.insert(dsu.findUPar(x*n+y));
                     }
                 }
-                int sizeTotal = 0;
-                for (auto it : components) {
-                    sizeTotal += ds.size[it];
+                int size=0;
+                for(auto it:components){
+                    size+= dsu.size[it];
                 }
-                mx = max(mx, sizeTotal + 1);
+                maxi=max(maxi,size+1);
             }
         }
-        for (int cellNo = 0; cellNo < n * n; cellNo++) {
-            mx = max(mx, ds.size[ds.findUPar(cellNo)]);
+        for(int i=0;i<n*n;i++){
+            maxi=max(maxi,dsu.size[dsu.findUPar(i)]);
         }
-        return mx;
+        return maxi;
+
     }
 };
