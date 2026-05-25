9d5035b8228b06444a156b1db6ec99c51ffdb6fe
prashantbhadu <prashantbhadu97@gmail.com>
2026-05-26 02:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (55.27%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    int solve(int i,int j,int n,vector<vector<int>>&arr){
-        if(i==n-1) return arr[i][j];
-        int down=1e9;
-        int right=1e9;
-        down=arr[i][j]+solve(i+1,j,n,arr);
-        if(j<=i+1){
-            right=arr[i][j]+solve(i+1,j+1,n,arr);
-        }
-        return min(down,right);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle[n-1][j];
+        int m=triangle[n-1].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        for(int i=0;i<m;i++){
+            dp[n-1][i]=triangle[n-1][i];
         }
         for(int i=n-2;i>=0;i--){
             for(int j=i;j>=0;j--){
-                int down=triangle[i][j] + dp[i+1][j];
-                int diag=triangle[i][j] + dp[i+1][j+1];
-                dp[i][j]=min(down,diag);
+                int same=triangle[i][j] + dp[i+1][j];
+                int diag=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(same,diag);
             }
         }
         return dp[0][0];
