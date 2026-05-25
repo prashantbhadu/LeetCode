70a67bcf18209b3b3f6b3f128bb3f7fa546f4c0b
prashantbhadu <prashantbhadu97@gmail.com>
2026-05-26 02:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (56.77%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,17 +3,20 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         int m=triangle[n-1].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
+        // vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<int>prev(m,0);
         for(int i=0;i<m;i++){
-            dp[n-1][i]=triangle[n-1][i];
+            prev[i]=triangle[n-1][i];
         }
         for(int i=n-2;i>=0;i--){
+            vector<int>temp(m,0);
             for(int j=i;j>=0;j--){
-                int same=triangle[i][j] + dp[i+1][j];
-                int diag=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(same,diag);
+                int same=triangle[i][j] + prev[j];
+                int diag=triangle[i][j]+prev[j+1];
+                temp[j]=min(same,diag);
             }
+            prev=temp;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
