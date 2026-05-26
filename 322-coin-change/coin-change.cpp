b240a616305d0cbff44912ac8330a084dfa5881f
prashantbhadu <prashantbhadu97@gmail.com>
2026-05-26 18:00:55 +0530

    Time: 48 ms (25.86%) | Memory: 49 MB (32.53%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    int solve(int ind, int k, vector<int>&coins,vector<vector<int>>&dp){
-        if(ind==0){
-            if(k%coins[ind]==0) return k/coins[ind];
-            return 1e9;
-        }
-        if(dp[ind][k]!=-1) return dp[ind][k];
-        int pick=1e9;
-        if(k>=coins[ind]){
-            pick=solve(ind,k-coins[ind],coins,dp)+1;
-        }
-        int not_pick=solve(ind-1,k,coins,dp)+0;
-        return dp[ind][k]=min(pick,not_pick);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        if(amount ==0) return 0;
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-0));
+        if (amount==0) return 0;
+        vector<vector<int>>dp(n,vector<int>(amount+1,1e9));
         for(int i=0;i<=amount;i++){
             if(i%coins[0]==0) dp[0][i]=i/coins[0];
-            else dp[0][i]=1e9;
         }
-        for(int ind=1;ind<n;ind++){
-            for(int k=0;k<=amount;k++){
-                int pick=1e9;
-                if(k>=coins[ind]) pick=dp[ind][k-coins[ind]]+1;
-                int not_pick=dp[ind-1][k]+0;
-                dp[ind][k]=min(pick,not_pick);
+        for(int i=1;i<n;i++){
+            for(int target=0;target<=amount;target++){
+                int stay=1e9;
+                if(target>=coins[i]){
+                    stay=1+dp[i][target-coins[i]];
+                }
+                int move=dp[i-1][target];
+                dp[i][target]=min(stay,move);
             }
         }
-        if(dp[n-1][amount]>=1e9) return -1;
-        return dp[n-1][amount];
+        return dp[n-1][amount]>=1e9?-1:dp[n-1][amount];
     }
 };
