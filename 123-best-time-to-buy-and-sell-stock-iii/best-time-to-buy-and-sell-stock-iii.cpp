9c90e6b2c77e877bb949d0de49fd2f33b1d9d5de
prashantbhadu <prashantbhadu97@gmail.com>
2026-05-29 13:11:55 +0530

    Time: 390 ms (46.16%) | Memory: 211.6 MB (31.03%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
-    int solve(int ind,int buy,int n,vector<int>&arr,int rem,vector<vector<vector<int>>>&dp){
-        if(ind==n) return 0;
-        if(rem==2) return 0;
-        int profit=-1e9;
-        int not_profit=-1e9;
-        if(dp[ind][buy][rem]!=-1) return dp[ind][buy][rem];
-        if(buy==1){
-            profit=-arr[ind]+solve(ind+1,0,n,arr,rem,dp);
-            not_profit=solve(ind+1,1,n,arr,rem,dp);
-        }
-        else{
-            profit=arr[ind]+solve(ind+1,1,n,arr,rem+1,dp);
-            not_profit=solve(ind+1,0,n,arr,rem,dp);
-        }
-        return max(profit,not_profit);
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>after(2,vector<int>(3,0));
-        vector<vector<int>>curr(2,vector<int>(3,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                for(int cap=1;cap<3;cap++){
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int k=1;k>=0;k--){
                     int profit=-1e9;
                     int not_profit=-1e9;
-                    if(buy==1){
-                        curr[buy][cap]= max( -prices[ind]+after[0][cap],after[1][cap]);
-                        
+                    if(j==0){
+                        profit=-prices[i]+dp[i+1][1][k];
+                        not_profit=dp[i+1][0][k];
                     }
                     else{
-                       curr[buy][cap]=max(prices[ind]+after[1][cap-1],after[0][cap]);
+                        profit=prices[i]+dp[i+1][0][k+1];
+                        not_profit=dp[i+1][1][k];
                     }
+                    dp[i][j][k]=max(profit,not_profit);
                 }
             }
-            after=curr;
         }
-        return after[1][2];
+        return dp[0][0][0];
     }
 };
