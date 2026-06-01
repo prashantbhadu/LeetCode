e45d253ccc8190850f71b8fb17e1da5c895c1e23
prashantbhadu <prashantbhadu97@gmail.com>
2026-06-02 01:52:36 +0530

    Time: 12 ms (51.06%) | Memory: 16.7 MB (56.53%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -4,14 +4,17 @@ public:
         int n=word1.size();
         int m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        int ans=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
+        cout<<ans<<endl;
+        
         return (n-dp[n][m])+(m-dp[n][m]);
     }
 };
