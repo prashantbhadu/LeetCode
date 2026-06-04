1bc77fbffbaca0873735321d467b65c960f3ec49
prashantbhadu <prashantbhadu97@gmail.com>
2026-06-04 12:35:19 +0530

    Time: 4 ms (68.69%) | Memory: 238.1 MB (43.81%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int n = landStartTime.size(), k = waterStartTime.size();
-        //path A
-        int cta=INT_MAX;
-        for(int i=0; i<n; i++){
-            int tr = landStartTime[i]+landDuration[i];
-            if(tr < cta){
-                cta = tr;
-            }
+        int n=landStartTime.size();
+        int m=waterStartTime.size();
+        int kimo=INT_MAX;
+        for(int i=0;i<n;i++){
+            int tr=landStartTime[i]+landDuration[i];
+            kimo=min(kimo,tr);
         }
-        int tr=INT_MAX;
-        for(int i=0; i<k; i++){
-            if(waterStartTime[i]<=cta) tr=min(tr,waterDuration[i]);
+        int lmao=INT_MAX;
+        for(int i=0;i<m;i++){
+            if(waterStartTime[i]<=kimo) lmao=min(lmao,waterDuration[i]);
             else{
-                tr = min(tr,waterStartTime[i]-cta+waterDuration[i]);
+                lmao=min(lmao,waterStartTime[i]-kimo + waterDuration[i]);
             }
         }
-        cta+=tr;
-        //path B
-        int ctb=INT_MAX;
-        for(int i=0; i<k; i++){
-            int tr = waterStartTime[i]+waterDuration[i];
-            if(tr < ctb){
-                ctb = tr;
-            }
+        kimo+=lmao;
+        int kimo2=INT_MAX;
+        for(int i=0;i<m;i++){
+            int pr=waterStartTime[i]+waterDuration[i];
+            kimo2=min(kimo2,pr);
         }
-        tr=INT_MAX;
-        for(int i=0; i<n; i++){
-            if(landStartTime[i]<=ctb) tr=min(tr,landDuration[i]);
+        int lmao2=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(landStartTime[i]<=kimo2) lmao2=min(lmao2,landDuration[i]);
             else{
-                tr = min(tr,landStartTime[i]-ctb+landDuration[i]);
+                lmao2=min(lmao2,landStartTime[i]-kimo2+landDuration[i]);
             }
         }
-        ctb+=tr;
-        return min(cta,ctb);
+        kimo2+=lmao2;
+        int ans=min(kimo,kimo2);
+        return ans;
     }
 };
