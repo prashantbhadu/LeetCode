3f4a97061242af7b8723208f28f3a3a2df1b81af
prashantbhadu <prashantbhadu97@gmail.com>
2026-06-08 21:23:16 +0530

    Time: 0 ms (100.00%) | Memory: 133.4 MB (62.93%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        queue<int>less;
-        queue<int>great;
-        int cnt_pivot=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>pivot){
-                great.push(nums[i]);
-            }
-            else if(nums[i]<pivot){
-                less.push(nums[i]);
-            }
-            else cnt_pivot++;
-        }
-        cout<<cnt_pivot<<endl;
         vector<int>ans;
-        while(!less.empty()){
-            int val=less.front();
-            less.pop();
-            ans.push_back(val);
+        for(auto val:nums){
+            if(val<pivot) ans.push_back(val);
         }
-        while(cnt_pivot>0){
-            ans.push_back(pivot);
-            cnt_pivot--;
+        for(auto val:nums){
+            if(val==pivot) ans.push_back(val);
         }
-        while(!great.empty()){
-            int val=great.front();
-            great.pop();
-            ans.push_back(val);
+        for(auto val:nums){
+            if(val>pivot) ans.push_back(val);
         }
         return ans;
     }
