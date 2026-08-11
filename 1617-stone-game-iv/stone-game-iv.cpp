b6869308b37835635c17c64dd27a5171415c1e19
prashantbhadu <prashantbhadu97@gmail.com>
2026-08-12 00:00:18 +0530

    Time: 121 ms (22.88%) | Memory: 22.6 MB (12.56%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
-    bool func(int n,vector<int>&dp){
-       if(n==0) return false;
-       if(dp[n]!=-1) return dp[n];
-       for(int i=1;i*i<=n;i++){
-        if(func(n-i*i,dp)==false) return dp[n]=true;
-       }
-       return dp[n]=false;
+    bool func(int n,vector<vector<int>>&dp,int flag){
+        if(n==0){
+            if(flag==0) return false;
+            else return true;
+        }
+        if(dp[flag][n]!=-1) return dp[flag][n];
+        if(flag==0){
+            for(int i=1;i*i<=n;i++){
+                if(func(n-i*i,dp,1)) return dp[flag][n]=true;
+            }
+            return dp[flag][n]=false;
+        }
+        else{
+            for(int i=1;i*i<=n;i++){
+                if(func(n-i*i,dp,0)) continue;
+                return dp[flag][n]=false;
+            }
+           
+
+        }
+        return dp[flag][n]=true;;
+
     }
     bool winnerSquareGame(int n) {
-        vector<int>dp(n+1,-1);
-        return func(n,dp);
+        vector<vector<int>>dp(2,vector<int>(n+1,-1));
+        return func(n,dp,0);
     }
 };
