03f97cd3b919a023cacd1bd117f950e91228fa43
prashantbhadu <prashantbhadu97@gmail.com>
2026-08-22 02:18:40 +0530

    Time: 60 ms (34.73%) | Memory: 15.7 MB (67.10%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    bool func(int i, int j, string s, string p){
+        if(i<0 && j<0) return true;
+        if(i<0 && j>=0){
+            for(int k=0;k<=j;k++){
+                if(p[k]!='*') return false;
+            }
+            return true;
+        }
+        if(j<0) return false;
+        if(s[i]==p[j] || p[j]=='?'){
+            return func(i-1,j-1,s,p);
+        }
+        if(p[j]=='*'){
+           return func(i-1,j,s,p) || func(i,j-1,s,p);
+        }
+        return false;
+    }
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
@@ -17,7 +34,9 @@ public:
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
                 else if(p[j-1]=='*'){
                     dp[i][j]= (dp[i-1][j] || dp[i][j-1]);
                 }
