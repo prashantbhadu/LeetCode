72300ca289fd082af0c7cfcdc46e7f1c41063cc0
prashantbhadu <prashantbhadu97@gmail.com>
2026-08-22 01:42:29 +0530

    Time: 5 ms (73.18%) | Memory: 13.3 MB (43.93%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
+    int func(int i,int j,string word1,string word2){
+        if(i<0 ) return j+1;
+        if(j<0 ) return i+1;
+        if(word1[i]==word2[j]) return func(i-1,j-1,word1,word2);
+        return min({1+func(i-1,j,word1,word2),1+func(i-1,j-1,word1,word2),1+func(i,j-1,word1,word2)});
+    }
     int minDistance(string word1, string word2) {
         int n=word1.size();
-        int  m=word2.size();
+        int m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        dp[0][0]=0;
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<=n;i++){
             dp[i][0]=i;
         }
-        for(int i=1;i<=m;i++){
+        for(int i=0;i<=m;i++){
             dp[0][i]=i;
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
+                int ans=1e9;
+                int ans1=1e9;
                 if(word1[i-1]==word2[j-1]){
                     dp[i][j]=dp[i-1][j-1];
                 }
-                else{
-                    dp[i][j]=min({1+dp[i-1][j-1],1+dp[i][j-1],1+dp[i-1][j]});
-                }
+                else dp[i][j]=min({1+dp[i-1][j-1],1+dp[i-1][j],1+dp[i][j-1]});
+                
             }
         }
         return dp[n][m];
