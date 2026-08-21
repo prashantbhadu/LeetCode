dfd4c3887653b56081169293b0746955ce29b5cf
prashantbhadu <prashantbhadu97@gmail.com>
2026-08-21 23:25:41 +0530

    Time: 17 ms (18.85%) | Memory: 17 MB (16.43%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
+         string s2=word2;
+        string s=word1;
+        int n=s.size();
+        int m=s2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        int ans=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                int ans=0;
+                int ans2=0;
+                if(s[i-1]==s2[j-1]){
+                    ans=1+dp[i-1][j-1];
                 }
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                else {
+                    ans2=max(dp[i-1][j],dp[i][j-1]);
+                }
+                dp[i][j]=max(ans,ans2);
             }
         }
-        cout<<ans<<endl;
-        
         return (n-dp[n][m])+(m-dp[n][m]);
     }
 };
