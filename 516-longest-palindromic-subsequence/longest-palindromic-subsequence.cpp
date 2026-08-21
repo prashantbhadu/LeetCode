9e41bbfe5989fd1dd73dea3bb01345b9f24d5b6c
prashantbhadu <prashantbhadu97@gmail.com>
2026-08-21 20:36:09 +0530

    Time: 117 ms (5.01%) | Memory: 76 MB (22.98%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-    int func(int start,int end,string&s){
-        if(start>end) return 0;
-        int pick=-1e9;
-        if(s[start]==s[end]){
-            if(start==end){
-                return 1;
-            }
-            else{
-                pick=2+func(start+1,end-1,s);
-            }
-        }
-        int not_pick=max(func(start+1,end,s),func(start,end-1,s));
-        return max(pick,not_pick);
-    }
     int longestPalindromeSubseq(string s) {
-        string k=s;
-        reverse(k.begin(),k.end());
+        string s2=s;
+        reverse(s2.begin(),s2.end());
         int n=s.size();
-       // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-       vector<int>prev(n+1,0);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         for(int i=1;i<=n;i++){
-            vector<int>temp(n+1,0);
             for(int j=1;j<=n;j++){
-                if(s[i-1]==k[j-1]){
-                    temp[j]=1+prev[j-1];
+                int ans=0;
+                int ans2=0;
+                if(s[i-1]==s2[j-1]){
+                    ans=1+dp[i-1][j-1];
                 }
-                else temp[j]=max(prev[j],temp[j-1]);
+                else {
+                    ans2=max(dp[i-1][j],dp[i][j-1]);
+                }
+                dp[i][j]=max(ans,ans2);
             }
-            prev=temp;
         }
-        return prev[n];
+        return dp[n][n];
     }
 };
